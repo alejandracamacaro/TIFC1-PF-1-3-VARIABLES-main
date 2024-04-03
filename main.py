9af7990ca2796030935446055f34f822c4aecdcf
alejandracamacaro <mary.camacaro@genstudents.org>
2024-04-03 12:42:00 -0300

    tarea 2

---
+++ main.py
@@ -0,0 +1,11 @@
+shopping_list = []
+
+articulo_1 = input("Escoge el primer articulo: ")
+articulo_2 = input("Escoge el segundo articulo: ")
+articulo_3 = input("Escoge el tercer articulo: ")
+
+shopping_list.append(articulo_1)
+shopping_list.append(articulo_2)
+shopping_list.append(articulo_3)
+
+print(shopping_list)
